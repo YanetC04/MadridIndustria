5b785995d847a72204cf40cf197df79ba8b0361d
Alex <alexrodriguezmazariegos@gmail.com>
2024-01-08 21:47:24 +0100

    Splash finished and new folder recursos

--- app/src/main/java/com/example/madridindustria/Splash.java
+++ app/src/main/java/com/example/madridindustria/Splash.java
@@ -23,7 +23,7 @@ public class Splash extends AppCompatActivity {
 
         videoView.setOnCompletionListener(mp -> {
             // Call Activity Main when the animation ends
-            startActivity(new Intent(Splash.this, MainActivity.class));
+            startActivity(new Intent(Splash.this, Login.class));
         });
 
         // Start the animation
