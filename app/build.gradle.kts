0bb01306bb61028dd4b0922a768f4b18117b86ab
blue-c0de <cxsguillen@gmail.com>
2024-02-05 22:36:02 +0100

    Google Play Store App Release + Themes Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "proyectointegrador.madridindustria"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
