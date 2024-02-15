50b4ddaf657a5568a0b93cba8574383511d70cea
blue-c0de <cxsguillen@gmail.com>
2024-02-15 12:45:36 +0100

    Update Package

--- app/src/main/java/com/proyectointegrador/madridindustria/Add.java
+++ app/src/main/java/com/proyectointegrador/madridindustria/Add.java
@@ -1,23 +1,39 @@
 package com.proyectointegrador.madridindustria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
+import com.google.android.material.textfield.TextInputLayout;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Add extends AppCompatActivity {
 
-    private EditText nombreEditText, inaguracionEditText, patrimonioEditText,coordenadas_latEditText, coordenadas_lonEditText,  metroEditText, distritoEditText, direccionEditText, descripcionEditText;
+    private EditText nombreEditText, inaguracionEditText, patrimonioEditText,coordenadas_latEditText, coordenadas_lonEditText,  metroEditText, direccionEditText, descripcionEditText;
+    private TextInputLayout nombreInputLayout, inaguracionInputLayout, patrimonioInputLayout, coordenadas_latInputLayout, coordenadas_lonInputLayout, metroInputLayout, direccionInputLayout, descripcionInputLayout;
+    private String nombreText, inaguracionText, patrimonioText,coordenadas_latText, coordenadas_lonText,  metroText, direccionText, descripcionText, distritoText;
+    private Spinner distrito;
     private Button enviar;
+    private String dist = "";
+    private Drawable redBorderDrawable;
+    private Drawable defaultBorderDrawable;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,10 +47,18 @@ public class Add extends AppCompatActivity {
         coordenadas_latEditText = findViewById(R.id.coordenadas_lat);
         coordenadas_lonEditText = findViewById(R.id.coordenadas_lon);
         metroEditText = findViewById(R.id.metro);
-       // distritoEditText = findViewById(R.id.distrito);
+        distrito = findViewById(R.id.distrito);
         direccionEditText = findViewById(R.id.direccion);
         descripcionEditText = findViewById(R.id.descripcion);
-
+        nombreInputLayout = findViewById(R.id.input_nombre);
+        inaguracionInputLayout = findViewById(R.id.input_inaguracion);
+        patrimonioInputLayout = findViewById(R.id.input_patrimonio);
+        coordenadas_latInputLayout = findViewById(R.id.input_coordenadas_lat);
+        coordenadas_lonInputLayout = findViewById(R.id.input_coordenadas_lon);
+        metroInputLayout = findViewById(R.id.input_metro);
+        direccionInputLayout = findViewById(R.id.input_direccion);
+        redBorderDrawable = ContextCompat.getDrawable(this, R.drawable.red_border);
+        defaultBorderDrawable = ContextCompat.getDrawable(this, R.drawable.default_border);
 
         // CREAMOS UN ARRAY DE LOS DATOS QUE QUEREMOS QUE MUESTRE AL SPINNER
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -45,39 +69,85 @@ public class Add extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // ASIGNAMOS EL ADAPTADOR A NUESTRO SPINNER
-        //d.setAdapter(adapter);
+        distrito.setAdapter(adapter);
 
-        /**dias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
-                if (position!=0)
-                    mensa.setText("Seleccionaste " +parent.getItemAtPosition(position));
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                mensa.setText("");
-            }
-        });**/
-
+        // ENVIAR
         enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Obtener el texto de los EditText
-                String nombre = nombreEditText.getText().toString();
-                String inaguracion = inaguracionEditText.getText().toString();
-                String patrimonio = patrimonioEditText.getText().toString();
-                String coordenadas_lat = coordenadas_latEditText.getText().toString();
-                String coordinadas_lon = coordenadas_lonEditText.getText().toString();
-                String metro = metroEditText.getText().toString();
-                String distrito = distritoEditText.getText().toString();
-                String direccion = direccionEditText.getText().toString();
-                String descripcion = descripcionEditText.getText().toString();
+                nombreText = nombreEditText.getText().toString();
+                inaguracionText = inaguracionEditText.getText().toString();
+                patrimonioText = patrimonioEditText.getText().toString();
+                coordenadas_latText = coordenadas_latEditText.getText().toString();
+                coordenadas_lonText = coordenadas_lonEditText.getText().toString();
+                metroText = metroEditText.getText().toString();
+                direccionText = direccionEditText.getText().toString();
+                descripcionText = descripcionEditText.getText().toString();
+                distritoText = distrito.getSelectedItem().toString();
+                double latitude = 0.0;
+                double longitude = 0.0;
 
+                if (!coordenadas_latText.isEmpty() && !coordenadas_lonText.isEmpty()) {
+                    latitude = Double.parseDouble(coordenadas_latText);
+                    longitude = Double.parseDouble(coordenadas_lonText);
+                } else {
+                    if (coordenadas_latText.isEmpty()) {
+                        coordenadas_latInputLayout.setHint("");
+                        coordenadas_latEditText.setBackground(redBorderDrawable);
+                        coordenadas_latEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                            @Override
+                            public void onFocusChange(View v, boolean hasFocus) {
+                                if(!hasFocus){
+                                    coordenadas_latEditText.setBackground(defaultBorderDrawable);
+                                    coordenadas_latInputLayout.setHint(R.string.email);
+                                }
+                            }
+                        });
+                    }
+                }
+
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                if (!(nombreText.isEmpty() || inaguracionText.isEmpty() || patrimonioText.isEmpty() || metroText.isEmpty() || direccionText.isEmpty() || descripcionText.isEmpty() || distritoText.isEmpty())) {
+                    HashMap<String, Object> datos = new HashMap<>();
+                    datos.put("nombre", nombreText);
+                    datos.put("inaguracion", inaguracionText);
+                    datos.put("patrimonio", patrimonioText);
+                    datos.put("geo", new GeoPoint(latitude, longitude));
+                    datos.put("metro", metroText);
+                    datos.put("direccion", direccionText);
+                    datos.put("descripcion", descripcionText);
+                    datos.put("distrito", distritoText);
+
+                    if (!distritoText.isEmpty()) {
+                        String[] palabras = distritoText.split("\\s+");
+                        if (palabras.length >= 2) {
+                            dist = palabras[1].toLowerCase();
+                        }
+                    }
+
+                    getCount(count -> {
+                        if (count >= 0) {
+                            db.collection(dist).document(String.valueOf(count+1)).set(datos);
+                        }
+                    });
+                } else {
+                    /**if (coordenadas_latText.isEmpty()) {
+                        coordenadas_latInputLayout.setHint("");
+                        coordenadas_latEditText.setBackground(redBorderDrawable);
+                        coordenadas_latEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                            @Override
+                            public void onFocusChange(View v, boolean hasFocus) {
+                                if(!hasFocus){
+                                    coordenadas_latEditText.setBackground(defaultBorderDrawable);
+                                    coordenadas_latInputLayout.setHint(R.string.email);
+                                }
+                            }
+                        });
+                    }**/
+                }
             }
         });
-
-
 
         // BARRA INFERIOR
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -103,6 +173,18 @@ public class Add extends AppCompatActivity {
             }
 
             return true;
+        });
+    }
+
+    public void getCount(final CountCallback countCallback) {
+        FirebaseFirestore.getInstance().collection("favorites").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                int count = task.getResult().size();
+                countCallback.onCallback(count);
+            } else {
+                Log.e("FirestoreData", "Error getting document count: " + Objects.requireNonNull(task.getException()).getMessage());
+                countCallback.onCallback(-1); // Indicates an error
+            }
         });
     }
 
