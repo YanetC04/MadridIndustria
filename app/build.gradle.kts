27696d097fac6a32b5e1fff8d032a989c0693cd1
blue-c0de <cxsguillen@gmail.com>
2024-06-10 00:06:13 +0200

    QR Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.proyectointegrador.madridindustria"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.3"
+        versionCode = 10
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
